cf1f86039fd272ddf23fc1abe0e0a7df9076a834
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2022-09-11 17:35:39 +0300

    upload images

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
+import { getFirestore } from "firebase/firestore"
 import { firebaseConfig } from "./.private";
-
+import { getStorage } from "firebase/storage"
 
 const app = initializeApp(firebaseConfig)
 
 export const db = getFirestore(app)
+export const storage = getStorage(app)
